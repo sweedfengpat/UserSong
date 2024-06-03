0585142d127cfbe4c198bcd479997a60e3516776
sweedfengpat <patza30596@gmail.com>
2024-06-04 04:00:26 +0700

    Add :: import All entities

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [Users, Songs],
+  entities: [__dirname + '/entity/*.ts'],
   synchronize: false,
   logging: false,
 });
